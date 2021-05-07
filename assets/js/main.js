5aaea3727a2e23fa1e268374a33a49ca23956c23
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-07 18:20:47 +0200

    completato esercizio con 3 prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,56 @@
+//struzioni:
+//Creare un oggetto che descriva uno studente, con le seguenti proprietà:
+/* nome,
+cognome
+età. */
 var student = {
-    'name' : 'Ricardo',
-    lastName : 'Jara',
-    age : 2021 - 1996,
+    'name' : 'Andrea',
+    lastName : 'Sevilla',
+    age : 2021 - 1994,
 }
+//Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 for (var key in student) {
-    
-    console.log(key);
+    console.log(key, student[key]);
 }
+// Creare un array di oggetti di studenti.
+var otherStudents = [
+    {
+        'name' : 'Ricardo',
+        lastName : 'Jara',
+        age : 2021 - 1996
+    },
+    {
+        'name' : 'Eveling',
+        lastName : 'Castillon',
+        age : 2021 - 1999
+    },
+    {
+        'name' : 'Alessandra',
+        lastName : 'Gomez',
+        age : 2021 - 1994
+    }
+]
+//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+for(var i = 0; i < otherStudents.length; i++) {
+    var studentEl = otherStudents[i];
+
+    for(var key in studentEl) {
+        console.log(key, studentEl[key]);
+    }
+}
+//Dare la possibilità all’utente, attraverso 3 prompt(),
+//di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età
+
+console.log(otherStudents);
+var nomeUtente = prompt('Inserisci nome');
+var cognomeUtente = prompt('Inserisci cognome');
+var etaUtente = Number(prompt('Inserisci età'));
+
+var listaUtente = {
+    'name' : nomeUtente,
+    lastName : cognomeUtente,
+    age : etaUtente
+};
+
+otherStudents.push(listaUtente);
